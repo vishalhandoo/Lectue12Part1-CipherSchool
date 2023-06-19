045c487c77b69d7db34516cea9ec975ce9685bb3
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-19 16:03:19 +0530

    Add files via upload

---
+++ Lecture12Part1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    
+    int a;
+    float b;
+    int *p=&a;
+
+    cout<<"The address of a is : "<<&a<<endl;
+    cout<<"The address of b is : "<<&b<<endl;
+    cout<<"The address of p is : "<<p<<endl;
+
+
+    return 0;
+}
